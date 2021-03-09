6d4c2e2284eb7c5a123412c1a84f14b649c580e3
rubdevton <rubdevton@gmail.com>
2021-03-08 20:24:11 -0800

    Switch to monocdk

--- lib/shine-fox-stack.ts
+++ lib/shine-fox-stack.ts
@@ -1,7 +1,7 @@
-import * as cdk from '@aws-cdk/core';
+import { Construct, Stack, StackProps } from 'monocdk';
 
-export class ShineFoxStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+export class ShineFoxStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // The code that defines your stack goes here

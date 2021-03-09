7d313ab9a88275e75e78c75c1c396e56b010ef73
rubdevton <rubdevton@gmail.com>
2021-03-08 20:37:43 -0800

    Set spaces per indentation level to 4

--- lib/shine-fox-stack.ts
+++ lib/shine-fox-stack.ts
@@ -1,9 +1,9 @@
 import { Construct, Stack, StackProps } from 'monocdk';
 
 export class ShineFoxStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-  }
+        // The code that defines your stack goes here
+    }
 }
